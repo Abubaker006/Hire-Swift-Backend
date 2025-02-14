474912e9c68745338209b536b30768130fe8f543
Muhammad Abubaker Naeem <113193781+Abubaker006@users.noreply.github.com>
2025-02-15 01:03:43 +0500

    a small bug

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 //routes
 app.use("/api/auth", authRoutes);
